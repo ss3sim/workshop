5e1d55882757fc6fc0e110c6bfc5327798956142
kellijohnson <kellifayejohnson@gmail.com>
2017-06-29 03:30:57 -0700

    Update installation instructions with latest R version and fix grammar

--- install.R
+++ install.R
@@ -1,17 +1,17 @@
 ## Check that a new enough R version is being used. R>= 3.2
-if(version$major != "3" | as.numeric(version$minor) < 2)
-  stop('R version needs to be > 3.2')
+if(version$major != "3" | as.numeric(version$minor) < 3.3)
+  stop('R version needs to be > 3.3.3')
 
 ## Install the package and any dependencies from github
 install.packages('devtools')
 library(devtools)
-install_github('r4ss/r4ss')
+install_github('r4ss/r4ss', dependencies = TRUE)
 install_github('ss3sim/ss3sim', dependencies = TRUE)
 
 ## Make sure you can load all of these, if not the install them manually
 ## with install.packages()
-library(ss3sim)
 library(r4ss)
+library(ss3sim)
 library(doParallel)
 library(foreach)
 library(ggplot2)
@@ -29,10 +29,10 @@ run_ss3sim(iterations=1, scenarios='D0-F1-cod', case_folder='cases',
 
 ## Checks to see if it worked
 if(!file.exists('D0-F1-cod/1/om/Report.sso')){
-    stop("OM failed to run, contact Cole or Kelli")
+    stop("OM failed to run, contact Kelli")
 } else if(!file.exists('D0-F1-cod/1/em/Report.sso')){
-    stop("EM failed to run, contact Cole or Kelli")
+    stop("EM failed to run, contact Kelli")
 } else {
     message("OM and EM ran successfully: package installed and working!")
-    unlink('D0-F1-cod', TRUE)
+    unlink('D0-F1-cod', recursive = TRUE)
 }
